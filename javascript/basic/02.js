ca86e99d230e5bbc7773d92906cad9c967b9762d
akshayyyx18 <akshayyyx.18@gmail.com>
2026-06-27 11:06:26 +0530

    add new files

---
+++ javascript/basic/02.js
@@ -0,0 +1,16 @@
+"use strict"// treat all js code as new version
+//alert(3+3)// we r using node js this work in browser
+
+let name ="akshay"
+let age = 20
+let isLoggedIn=false
+
+//datatypes :
+// number =>2 to power 54
+// BigInt
+// string =>" "
+// boolean => true/false
+// null=> standalone value 
+// undefined => (input nhi diya h)
+// symbol=>unique
+// object
---
+++ javascript/basic/basic.js
@@ -0,0 +1,8 @@
+console.log("hii")
+const accountId ="123456"
+let accountGmail ="akshay@123"
+var accountPassword ="12345678"
+accountCity ="Gurugram";
+/*const is constant and can't change, let and var can be changed (use let than var */
+console.log(accountId);
+console.table([accountId,accountGmail,accountPassword,accountCity])
