3506dd1353f9f4ebc7eb10c447e9bca58ea444b2
Akshay Shakya <akshayyyx.18@gmail.com>
2026-07-31 18:45:19 +0530

    new files

--- javascript/basic/02.js
+++ javascript/basic/02.js
@@ -16,3 +16,14 @@
 // object
 let bill=18/100*(79990+2*24990)
 console.log('your final bill is '+ bill)
+
+// operator precedence  
+// 1. () => 2. ** => 3. * / % => 4. + - => 5. = += -= *= /=
+//scope of javascript
+// any variable defined inside a function is not accessible outside the function
+//variable can be defined inside a block and can be accessed outside the block if it is defined using var keyword
+//var does not follow scope
+//global scope => variable defined outside any function or block
+//block scope => variable defined inside a block
+//declare variablesin  the narrowest scope possible
+
