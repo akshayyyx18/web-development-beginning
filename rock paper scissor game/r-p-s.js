2e2e5084c8bd6f05738bb98af84d299f7e363ebd
Akshay Shakya <akshayyyx.18@gmail.com>
2026-08-30 17:12:38 +0530

    new

--- rock paper scissor game/r-p-s.js
+++ rock paper scissor game/r-p-s.js
@@ -6,7 +6,7 @@ const userScorePara= document.querySelector(".user-score");
 const compScorePara= document.querySelector(".comp-score");
 
 const genCompChoice = () =>{
-    const options=["Rock", "paper", "Scissor"];
+    const options=["Rock", "Paper", "Scissor"];
     const ranIndx=Math.floor(Math.random()*3);
     return options[ranIndx];
 }
